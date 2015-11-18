65a49e5ef3ed63ca8c371b2d249f51649a36493f
Spencer Wiltbank <spencerwiltbank@email.arizona.edu>
2015-11-18 13:17:43 -0700

    merge conflicts

--- src/model/resources/Resources.java
+++ src/model/resources/Resources.java
@@ -1,7 +1,7 @@
 package model.resources;
 
 public enum Resources {
-<<<<<<< HEAD
+	
 	COAL(1),
 	COPPER(2),
 	ELECTRICITY(3),
@@ -17,23 +17,6 @@ public enum Resources {
 	
 	public int getValue(){
 		return value;
-=======
-	COAL(0),
-	COPPER(1),
-	ELECTRICITY(2),
-	GOLD(3),
-	IRON(4),
-	OIL(5);
-	
-	private int identifier;
-	
-	private Resources(int i){
-		identifier = i;
-	}
-	
-	public int getIdentifer(){
-		return identifier;
->>>>>>> b3f63e43832b6031aabe5735eb74298b2e78b2e2
 	}
 	
 }
