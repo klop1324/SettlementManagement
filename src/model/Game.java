7f5c144b852d53a570f8ad44242181a3fa411b63
Miranda Motsinger <mirandamotsinger@email.arizona.edu>
2015-11-18 12:39:01 -0700

    Updated age nts

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,19 +1,28 @@
 package model;
 
-import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Observable;
+
+import javax.swing.Timer;
 
 import model.agents.AbstractAgent;
 import model.buildings.AbstractBuilding;
 import model.resources.AbstractResource;
+import model.resources.Resources;
 
 public class Game extends Observable {
 	
+	private Game game = this;
 	private ArrayList<AbstractBuilding> buildings;
 	private	ArrayList<AbstractResource> resources;
 	private ArrayList<AbstractAgent> agents;
 	private Map map;
+	private Timer timer;
+	private int[] currentResources;
 
 	public Game() {
 		this.map = new Map(GlobalSettings.MAP_SIZE_X, GlobalSettings.MAP_SIZE_Y);
@@ -21,7 +30,8 @@ public class Game extends Observable {
 		this.resources = new ArrayList<AbstractResource>();
 		this.agents = new ArrayList<AbstractAgent>();
 		
-		//TODO implement tick system
+		timer = new Timer(50, new TickActionListener());
+		
 		
 		//TODO intitial resource generation
 		
@@ -30,6 +40,10 @@ public class Game extends Observable {
 
 	public Map getMap() {
 		return map;
+	}
+	
+	public void BuildBuilding(AbstractBuilding b, Point p){
+		this.buildings.add(b);
 	}
 
 	public ArrayList<AbstractAgent> getAgents() {
@@ -44,5 +58,30 @@ public class Game extends Observable {
 		return buildings;
 	}
 	
+	private class TickActionListener implements ActionListener{
 
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			
+			timer = new Timer(50, new TickActionListener());
+			
+			//tick all the agents
+			for(int i = 0; i < agents.size(); i++){
+				agents.get(i).tic();
+			}
+			
+			//collect the resources for each building
+			for(int i = 0; i < buildings.size(); i++){
+				AbstractBuilding b = buildings.get(i);
+				if(b.isPassiveProvider()){
+					
+					
+				}
+			}
+			
+			
+			
+		}
+		
+	}
 }
--- src/model/GlobalSettings.java
+++ src/model/GlobalSettings.java
@@ -2,8 +2,8 @@ package model;
 
 public class GlobalSettings {
 	
-	public static int MAP_SIZE_X = 1024;
-	public static int MAP_SIZE_Y = 1024;
+	public static int MAP_SIZE_X = 100;
+	public static int MAP_SIZE_Y = 100;
 	//resource distribution settings to be here;
 	
 
