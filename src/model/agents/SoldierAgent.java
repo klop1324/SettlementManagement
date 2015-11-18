4f1cc9e32e302a0712d4ee682935cc7b8a01f255
Miranda Motsinger <mirandamotsinger@email.arizona.edu>
2015-11-17 17:33:25 -0700

    Updated age nts

--- src/model/agents/SoldierAgent.java
+++ src/model/agents/SoldierAgent.java
@@ -1,11 +1,29 @@
 package model.agents;
 
+import java.awt.Point;
+
 public class SoldierAgent extends AbstractAgent{
 
+	public SoldierAgent(Point position) {
+		super(position);
+		filename = "???";
+		textRep = 'S';
+		MAX_RESOURCES = 500;
+	}
+
 	@Override
-	public void doWork() {
-		// TODO Auto-generated method stub
-		
+	void decrementEnergy() {
+		energy = energy - 3;		
+	}
+
+	@Override
+	void decrementCondition() {
+		condition = condition - 3;	
+	}
+
+	@Override
+	void decrementOil() {
+		oil = oil - 3;
 	}
 
 }
