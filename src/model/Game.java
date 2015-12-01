10bd02e14328cb5c193eddb53c5063e71a91e0cc
Miranda Motsinger <mirandamotsinger@email.arizona.edu>
2015-11-30 19:26:31 -0700

    agentAddCapacity changed in Game

--- src/model/Game.java
+++ src/model/Game.java
@@ -95,7 +95,7 @@ public class Game extends Observable{
 		sendAgentsToBuilding(building);
 		for (AbstractAgent a: agents) {
 			if (a.getPosition().equals(building.getLocation())){
-				building.agentAddCapacity(r.getType(), a.getAmountCarried(), a);
+				building.agentAddCapacity(r.getType(), a.getAmountCarried());
 				collected = false;
 			}
 		}
