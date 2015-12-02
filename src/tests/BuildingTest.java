200482a9322d5544aaf46381f7cc0ea70d877f2f
Spencer Wiltbank <spencerwiltbank@email.arizona.edu>
2015-12-01 22:08:53 -0700

    fixed tests

--- src/tests/BuildingTest.java
+++ src/tests/BuildingTest.java
@@ -10,11 +10,7 @@ import model.Game;
 import model.agents.BuilderAgent;
 import model.agents.WorkerAgent;
 import model.buildings.Building;
-import model.buildings.Armory;
 import model.buildings.BuildingType;
-import model.buildings.ChargingStation;
-import model.buildings.HomeDepot;
-import model.buildings.JunkYard;
 import model.resources.Resource;
 import model.resources.ResourceType;
 
@@ -43,15 +39,15 @@ public class BuildingTest {
 	}
 	@Test
 	public void junkYardTest() {
-		JunkYard junkyard = new JunkYard(100, new Point(1,2));
+		Building junkyard = new Building(BuildingType.JUNKYARD, new Point(5,5));
 		Resource oil = new Resource(10, new Point(12, 3), ResourceType.OIL);
 		Resource iron = new Resource(10, new Point(12, 3), ResourceType.IRON);
-		assertFalse(junkyard.getResources().containsKey(oil.getType()));
-		assertTrue(junkyard.getResources().containsKey(iron.getType()));
+		assertFalse(junkyard.getResources().contains(oil.getType()));
+		assertTrue(junkyard.getResources().contains(iron.getType()));
 		junkyard.agentAddCapacity(iron.getType(), 20);
-		assertEquals((int)junkyard.getResources().get(iron.getType()), 20);
+		assertEquals((int)junkyard.getCapacity(), 30000);
 		junkyard.agentRemoveCapacity(iron.getType(), 10);
-		assertEquals((int)junkyard.getResources().get(iron.getType()), 10);
+		assertEquals((int)junkyard.getResourceAmount(ResourceType.IRON), 0);
 		System.out.println(junkyard.getResources());
 
 	}
@@ -59,25 +55,25 @@ public class BuildingTest {
 	@Test
 	public void chargingStationTest(){
 		Point point = new Point(13, 7);
-		ChargingStation charge = new ChargingStation(90, point);
+		Building charge = new Building(BuildingType.CHARGINGSTATION, new Point(5,5));
 		Resource electricSlide = new Resource(10, new Point(13,7), ResourceType.ELECTRICITY);
-		assertEquals((int) charge.getResources().get(electricSlide.getType()), 0);
+		assertEquals((int) charge.getResourceAmount(ResourceType.ELECTRICITY), 0);
 		charge.agentAddCapacity(electricSlide.getType(), 9);
-		assertEquals((int) charge.getResources().get(electricSlide.getType()), 9);
+		assertEquals((int) charge.getResourceAmount(electricSlide.getType()), 9);
 		charge.agentRemoveCapacity(electricSlide.getType(), 3);
-		assertEquals((int) charge.getResources().get(electricSlide.getType()), 6);
+		assertEquals((int) charge.getResourceAmount(electricSlide.getType()), 6);
 		charge.agentRemoveCapacity(electricSlide.getType(), 10000);
-		assertEquals((int) charge.getResources().get(electricSlide.getType()), 6);
+		assertEquals((int) charge.getResourceAmount(electricSlide.getType()), 6);
 		charge.agentAddCapacity(electricSlide.getType(), 10000);
-		assertEquals((int) charge.getResources().get(electricSlide.getType()), 6);
-		charge.passiveCheck();
+		assertEquals((int) charge.getResourceAmount(electricSlide.getType()), 6);
+		assertTrue(charge.isPassiveProvider());
 		System.out.println(ResourceType.COAL.name());
 	}
 	
 	@Test
 	public void locationCheck(){
 		Resource iron = new Resource(10, new Point(4, 30), ResourceType.IRON);
-		JunkYard junkyard = new JunkYard(40, new Point(30, 20));
+		Building junkyard = new Building(BuildingType.JUNKYARD, new Point(30,20));
 		assertEquals(iron.getLocation(), new Point(4, 30));
 		assertEquals(junkyard.getLocation(), new Point(30, 20));
 	}
