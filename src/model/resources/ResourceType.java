487c1b1c4dcd864a4569e81da88296b02b91db17
Spencer Wiltbank <spencerwiltbank@email.arizona.edu>
2015-12-02 13:18:59 -0700

    got resource generation working!

--- src/model/resources/ResourceType.java
+++ src/model/resources/ResourceType.java
@@ -2,22 +2,27 @@ package model.resources;
 
 public enum ResourceType {
 	
-	COAL(1),
-	COPPER(2),
-	ELECTRICITY(3),
-	GOLD(4),
-	IRON(5),
-	OIL(6),
-	TOOL(7);
+	COAL(1,true),
+	COPPER(2,true),
+	ELECTRICITY(3,false),
+	GOLD(4,true),
+	IRON(5,true),
+	OIL(6,true),
+	TOOL(7,false);
 	
 	private int value;
+	private boolean spawns;
 
-	ResourceType(int value){
+	ResourceType(int value, boolean spawns){
 		this.value = value;
+		this.spawns = spawns;
 	}
 	
 	public int getValue(){
 		return value;
+	}
+	public boolean isSpawnable(){
+		return spawns;
 	}
 	
 }
